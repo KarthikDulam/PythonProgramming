6e257cbf06d024b2aa2edddc2b3e51cad254b556
Karthik Dulam <karthikdulam@gmail.com>
2020-06-02 21:34:31 -0400

    June 2 Commits EOD

--- Leetcode/June2020_Problems/June1.py
+++ Leetcode/June2020_Problems/June1.py
@@ -1,1 +1,25 @@
-#test
+'''Invert Binary Tree'''
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def invertTree(self, root: TreeNode) -> TreeNode:
+        if root == None:
+            return None
+        stack = []
+        stack.append(root)
+        while len(stack) != 0:
+            t = stack[-1]
+            stack = stack[:-1]
+            temp = t.left
+            t.left = t.right
+            t.right = temp
+            if t.right != None:
+                stack.append(t.right)
+            if t.left != None:
+                stack.append(t.left)
+        return root
